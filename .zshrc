c635fe4f491922cc6ad8b82361bd71d63590d5d4
jollywho <works.kvollmer@gmail.com>
2014-06-16 22:01:45 -0400

    zsh

---
+++ .zshrc
@@ -0,0 +1,11 @@
+# Customize to your needs...
+
+for config_file ($HOME/zsh/*.zsh) source $config_file
+
+# emacs style
+bindkey '^a' beginning-of-line
+bindkey '^e' end-of-line
+bindkey '^n' down-history
+bindkey '^p' up-history
+bindkey '^u' kill-whole-line
+bindkey '^w' vi-backward-kill-word
---
+++ zsh/key-bindings.zsh
@@ -0,0 +1,14 @@
+# http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html
+# http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Zle-Builtins
+# http://zsh.sourceforge.net/Doc/Release/Zsh-Line-Editor.html#Standard-Widgets
+
+bindkey -v                                          # Use vi key bindings
+bindkey '^r' history-incremental-search-backward    # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
+
+# emacs style
+bindkey '^b' beginning-of-line
+bindkey '^e' end-of-line
+bindkey '^n' down-history
+bindkey '^p' up-history
+bindkey '^u' kill-whole-line
+bindkey '^w' vi-backward-kill-word
