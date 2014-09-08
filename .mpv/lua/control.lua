8966fd2169d909d9ed446f62b84c4e7b4fb561ae
Kevin Vollmer <works.kvollmer@gmail.com>
2014-09-08 15:09:41 -0400

    mpv controller working markers

--- .mpv/lua/control.lua
+++ .mpv/lua/control.lua
@@ -4,23 +4,30 @@ active = false
 
 function set_active()
   active = not active
+  print(active)
 end
 
 function set_mark()
+  active = false
   return tostring(mp.get_property("time-pos", ""))
 end
 
 function key_a()
   if active then
     markA = set_mark()
+  elseif not(markA == "") then
+    cmd = "seek " .. markA .. " absolute exact"
+    mp.command(cmd)
   end
   print(markA)
-  --mp.command("seek 21 relative exact")
 end
 
 function key_A()
   if active then
     markB = set_mark()
+  elseif not(markB == "") then
+    cmd = "seek " .. markB .. " absolute exact"
+    mp.command(cmd)
   end
   print(markB)
 end
