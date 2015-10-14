49427ca3b48bfe6f0704eb8b734ea9dca7d8e77e
Kevin Vollmer <works.kvollmer@gmail.com>
2015-10-14 05:07:17 -0400

    xorg stuff

--- scripts/格好いい/mpv_i.sh
+++ scripts/格好いい/mpv_i.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/bash
 # Workaround for ranger to use
 # a single mpv instance
+
 pkill -x mpv
+
+# use compton because fail drivers
+compton --config /dev/null --backend glx
+
 mpv -fs -- "$@"
